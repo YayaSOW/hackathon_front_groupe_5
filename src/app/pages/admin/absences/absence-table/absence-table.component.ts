dab5a45ea14347b950e2c28accc62a9d770cc840
Aicha Akonde <aichamarie5055@gmail.com>
2025-05-31 14:12:31 +0000

    Creation du composant absence item. Mise a jour routage. Mise a jour des composants

--- src/app/pages/admin/absences/absence-table/absence-table.component.ts
+++ src/app/pages/admin/absences/absence-table/absence-table.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-absence-table',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './absence-table.component.html',
   styleUrl: './absence-table.component.css'
 })
 export class AbsenceTableComponent {
 
+  constructor(private router: Router){}
+
+  onViewDetails(){
+    this.router.navigateByUrl('item');
+  }
 }
