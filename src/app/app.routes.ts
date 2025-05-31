d79be080442830918ac979b58c17b14fd3cf2bac
Aicha Akonde <aichamarie5055@gmail.com>
2025-05-31 16:06:55 +0000

    Mise a jour commit precedent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { EtudiantListPageComponent } from './pages/admin/etudiants/etudiant-list-page/etudiant-list-page.component';
 import { AbsenceListPageComponent } from './pages/admin/absences/absence-list-page/absence-list-page.component';
 import { RetardListPageComponent } from './pages/admin/retards/retard-list-page/retard-list-page.component';
+import { AbsenceItemComponent } from './pages/admin/absences/absence-item/absence-item.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'absence',
         component: AbsenceListPageComponent
+    },
+    {
+        path: 'item',
+        component: AbsenceItemComponent
     },
     {
         path: 'retard',
